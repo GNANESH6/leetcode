564e990309fd9157a5f8e42af46d374f353f1b05
GNANESHWAR REDDY SANGATI <sangatignaneshwarreddy2006@gmail.com>
2026-06-02 19:53:45 +0530

    Time: 33 ms (32.88%) | Memory: 152.7 MB (31.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n=nums.length;
+        int n=nums.length, idx=0;
         int[] ans= new int[n-k+1];
        Deque<Integer>dq=new LinkedList<>();
        for(int i=0;i<n;i++){
@@ -12,7 +12,7 @@ class Solution {
         }
         dq.addLast(i);
         if (i>=k-1) {
-                ans[i-k+1]=nums[dq.peekFirst()];
+                ans[idx++]=nums[dq.peekFirst()];
             }
        }
        return ans;
