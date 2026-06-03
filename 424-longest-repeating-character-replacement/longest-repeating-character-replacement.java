1e0520338bbbcffb35a4bcfd35d9275f0757ef4f
GNANESHWAR REDDY SANGATI <sangatignaneshwarreddy2006@gmail.com>
2026-06-03 10:55:27 +0530

    Time: 28 ms (23.16%) | Memory: 47.2 MB (11.22%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,14 +1,12 @@
-import java.util.Collection;
-
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l=0,r=0,max=0,n=s.length(),mostfreq=0;
+        int l=0,r=0,n=s.length(),max=0,mostFre=0;
         HashMap<Character,Integer> map=new HashMap<>();
         for(;r<n;r++){
             char ch=s.charAt(r);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-            mostfreq = Math.max(mostfreq, map.get(ch));
-            while( (r-l+1-mostfreq) >k){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            mostFre=Math.max(mostFre,map.get(ch));
+            while(r-l+1-mostFre>k){
                 char h=s.charAt(l);
                 map.put(h,map.getOrDefault(h,0)-1);
                 if(map.get(h)==0) map.remove(h);
@@ -17,6 +15,5 @@ class Solution {
             max=Math.max(max,r-l+1);
         }
         return max;
-    } 
-
+    }
 }
