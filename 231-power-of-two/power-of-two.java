0087394db6c47802ec910d2c625cddc1c578441b
GNANESHWAR REDDY SANGATI <sangatignaneshwarreddy2006@gmail.com>
2026-06-20 16:23:09 +0530

    Time: 1 ms (96.46%) | Memory: 42.6 MB (54.06%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+      if(n==1) return true;
+      if(n<=0 || n%2!=0) return false;
+    //   while(n%2==0){
+    //     n=n/2;
+    //     if(n==0 || n==1) return true;
+    //   }  
+    //   return false;
+    return (n&(n-1))==0;
+    }
+}
