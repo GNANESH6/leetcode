5503081204e0a24c683e606652d593d8d3181105
GNANESHWAR REDDY SANGATI <sangatignaneshwarreddy2006@gmail.com>
2026-06-21 07:04:58 +0530

    Time: 29 ms (81.54%) | Memory: 48.2 MB (43.15%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            if(sb.length()>0 && ch=='*') sb.deleteCharAt(sb.length()-1);
+            else sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
